99254d68d5e19f0d672da0e304436aa76cb36649
cobycloud <25079070+cobycloud@users.noreply.github.com>
2026-05-04 01:29:37 -0500

    Prepare release version bumps

--- pkgs/engines/tigrbl_engine_inmemcache/src/tigrbl_engine_inmemcache/__init__.py
+++ pkgs/engines/tigrbl_engine_inmemcache/src/tigrbl_engine_inmemcache/__init__.py
@@ -3,4 +3,4 @@
 from .plugin import register
 
 __all__ = ["register", "__version__"]
-__version__ = "0.1.0"
+__version__ = "0.4.0.dev1"
--- pkgs/engines/tigrbl_engine_inmemory/src/tigrbl_engine_inmemory/__init__.py
+++ pkgs/engines/tigrbl_engine_inmemory/src/tigrbl_engine_inmemory/__init__.py
@@ -4,4 +4,4 @@ from .rust import register_rust_engine
 from .plugin import register
 
 __all__ = ["register", "register_rust_engine", "__version__"]
-__version__ = "0.1.0"
+__version__ = "0.4.0.dev1"
