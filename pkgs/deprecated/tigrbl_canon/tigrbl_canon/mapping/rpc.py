f900d20907f25fe25d32e25ec2c3c2b3a0d471af
cobycloud <25079070+cobycloud@users.noreply.github.com>
2026-06-29 04:03:31 -0500

    Remove deprecated concrete RPC mapping shim

--- pkgs/deprecated/tigrbl_canon/tigrbl_canon/mapping/rpc.py
+++ pkgs/deprecated/tigrbl_canon/tigrbl_canon/mapping/rpc.py
@@ -7,10 +7,12 @@ _warn_deprecated_import(__name__)
 
 from warnings import warn
 
-from tigrbl_concrete._mapping.rpc import register_and_attach, rpc_call
+from tigrbl_base._base._rpc_map import register_and_attach
+from tigrbl_concrete._mapping.router.rpc import rpc_call
 
 warn(
-    "tigrbl_canon.mapping.rpc is deprecated; use tigrbl_concrete._mapping.rpc",
+    "tigrbl_canon.mapping.rpc is deprecated; use tigrbl_base._base._rpc_map "
+    "for register_and_attach and tigrbl_concrete._mapping.router.rpc for rpc_call",
     DeprecationWarning,
     stacklevel=2,
 )
